e0e096b86c77e100d2924782b8cf2c79c800389d
william Ramirez <willramirez630@gmail.com>
2024-08-03 21:52:37 -0400

    wip

--- src/popup/App.jsx
+++ src/popup/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div>
-      This extension rquires you to navigate to facebook.com's library and
-      login.
+    <div className="p-4">
+      This extension requires you to navigate to facebook.com's library and be
+      logged in.
     </div>
   );
 }
